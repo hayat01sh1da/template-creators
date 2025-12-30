d64d134d466ca54c662035036d9245fc993e378c
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2025-12-31 03:57:39 +0900

    Simplify passing parameters to application without conditional statements

--- ruby/main.rb
+++ ruby/main.rb
@@ -9,18 +9,6 @@ unit = gets.chomp.strip
 puts 'Provide the specific year you would like to create working report templates for(Default: the current year)'
 year = gets.chomp.strip
 
-username = _username.empty? ? nil : _username
-unit     = _unit.empty? ? nil : _unit
-year     = _year.empty? ? nil : _year
+params = { username:, unit:, year: }.reject { |_, value| value.empty? }
 
-if username && unit && year
-  Application.run(username:, unit:, year:)
-elsif username
-  Application.run(username:)
-elsif unit
-  Application.run(unit:)
-elsif year
-  Application.run(year:)
-else
-  Application.run
-end
+Application.run(**params)
