835963a8d072d5463d6f496aab72afd19eaa1a20
hayat01sh1da <bin.lhkty.gtthj.o.zs+github@gmail.com>
2026-04-17 03:26:01 +0900

    Fix: [error] Type `(::String | nil)` does not have method `chomp`

--- ruby/main.rb
+++ ruby/main.rb
@@ -3,13 +3,13 @@
 require_relative './src/application'
 
 puts 'Provide your username(Default: hayat01sh1da)'
-username = gets.chomp.strip
+username = gets&.chomp&.strip
 
 puts 'Provide your preferred unit d(daily - default), w(weekly) or m(monthly)'
-unit = gets.chomp.strip
+unit = gets&.chomp&.strip
 
 puts 'Provide the specific year you would like to create working report templates for(Default: the current year)'
-year = gets.chomp.strip
+year = gets&.chomp&.strip
 
 params = { username:, unit:, year: }.reject { |_, value| value.empty? }
 
